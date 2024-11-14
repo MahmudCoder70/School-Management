21eb537091fa055f38658a71fe69a4081d34a201
HP <HP@DESKTOP-NHNG90M>
2024-11-14 17:06:31 +0600

    trying to solve guardian cmp prob

--- src/app/components/RoutineConfiguration/create-configuration/create-configuration.component.ts
+++ src/app/components/RoutineConfiguration/create-configuration/create-configuration.component.ts
@@ -1,6 +1,45 @@
+// import { CommonModule } from '@angular/common';
+// import { HttpClient } from '@angular/common/http';
+// import { Component } from '@angular/core';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-create-configuration',
+//   standalone: true,
+//   imports: [CommonModule, ReactiveFormsModule, FormsModule],
+//   templateUrl: './create-configuration.component.html',
+//   styleUrl: './create-configuration.component.css',
+// })
+// export class CreateConfigurationComponent {
+//   ConfigName: string = '';
+//   ConfigValue: string = '';
+//   Campus: any = '';
+
+//   constructor(private http: HttpClient, private router: Router) {}
+
+//   AddConfiguration() {
+//     const formData = new FormData();
+//     formData.append('ConfigName', this.ConfigName);
+//     formData.append('ConfigValue', this.ConfigValue);
+//     formData.append('CampusId', this.Campus);
+
+//     this.http
+//       .post('http://localhost:5028/api/RoutineConfigurations', formData)
+//       .subscribe({
+//         next: (data) => {
+//           this.router.navigate(['ViewConfiguration']);
+//         },
+//         error: (err) => {
+//           console.error('Error creating Configuration:', err);
+//         },
+//       });
+//   }
+// }
+
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -11,18 +50,31 @@ import { Router } from '@angular/router';
   templateUrl: './create-configuration.component.html',
   styleUrl: './create-configuration.component.css',
 })
-export class CreateConfigurationComponent {
+export class CreateConfigurationComponent implements OnInit {
   ConfigName: string = '';
   ConfigValue: string = '';
-  Campus: any = '';
+  Campus: any[] = []; // Updated to hold an array of campuses
+  selectedCampus: string = ''; // To hold selected campus ID
 
   constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    // Fetch campus data when component loads
+    this.http.get<any[]>('http://localhost:5028/api/Campus').subscribe({
+      next: (data) => {
+        this.Campus = data; // Assign data to Campus array
+      },
+      error: (err) => {
+        console.error('Error loading campuses:', err);
+      },
+    });
+  }
 
   AddConfiguration() {
     const formData = new FormData();
     formData.append('ConfigName', this.ConfigName);
     formData.append('ConfigValue', this.ConfigValue);
-    formData.append('CampusId', this.Campus);
+    formData.append('CampusId', this.selectedCampus);
 
     this.http
       .post('http://localhost:5028/api/RoutineConfigurations', formData)
